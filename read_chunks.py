7ccb62fe7df58f0b243953b87bcaa509b322dc58
Jonathan Wright <jonathan.wright@gmail.com>
2021-05-26 15:04:49 +0200

    lz4 blocks decodes lz4

--- read_chunks.py
+++ read_chunks.py
@@ -1,6 +1,7 @@
 
 
-import h5py, hdf5plugin
+import h5py, hdf5plugin, struct
+import numpy as np, bslz4decoders
 
 def get_frame_h5py( h5name, dset, frame ):
     with h5py.File(h5name, "r") as h5f:
@@ -14,7 +15,18 @@ def get_chunk( h5name, dset, frame ):
         assert dset.chunks[0] == 1
         assert dset.chunks[1] == dset.shape[1]
         assert dset.chunks[2] == dset.shape[2]
-        return dset.id.read_direct_chunk( (frame, 0, 0) ), dset.shape, dset.dtype
+        return dset.id.read_direct_chunk( (frame, 0, 0) ), (dset.shape[1], dset.shape[2]), dset.dtype
+
+def get_blocks( chunk, shp, dtyp ):
+    total_output_size, blocksize = struct.unpack_from( "!QL", chunk, 0 )
+    print(total_output_size, blocksize)
+    assert total_output_size == shp[0]*shp[1]*dtyp.itemsize
+    if blocksize == 0:
+        blocksize = 8192
+    nblocks = (total_output_size + blocksize - 1 ) // ( blocksize )
+    blocks = np.empty( nblocks, dtype = np.uint32 )
+    bslz4decoders.read_starts( chunk, dtyp.itemsize, blocksize, blocks )
+    return blocksize, blocks
 
 def get_frames_h5py( h5name, dset ):
     with h5py.File(h5name, "r") as h5f:
@@ -29,7 +41,7 @@ def get_chunks( h5name, dset ):
         assert dset.chunks[1] == dset.shape[1]
         assert dset.chunks[2] == dset.shape[2]
         for frame in range(dset.shape[0]):
-            yield dset.id.read_direct_chunk( (frame, 0, 0) ), dset.shape, dset.dtype
+            yield dset.id.read_direct_chunk( (frame, 0, 0) ), (dset.shape[1], dset.shape[2]), dset.dtype
 
 def bench( func, *args ):
     start = timeit.default_timer()
--- test_decoders.py
+++ test_decoders.py
@@ -11,11 +11,16 @@ testcases = [
 
 def runtest_lz4chunkdecoders( decoder ):
     for h5name, dset in testcases:
+        # reference data
         ref = read_chunks.get_frame_h5py( h5name, dset, 0 )
+        # compressed data
         (filtered, byteschunk), shp, dtyp = read_chunks.get_chunk( h5name, dset, 0 )
         chunk = np.frombuffer(byteschunk, np.uint8)
-        out = np.empty( shp[1]*shp[2], dtyp )
+        # allocate output
+        out = np.empty( shp[0]*shp[1], dtyp )
+        # decode it for lz4
         decoder( chunk, dtyp.itemsize, out.view( np.uint8 ) )
+        # bitshuffle it
         decoded = bitshuffle.bitunshuffle( out )
         if not (decoded == ref.ravel()).all():
             print("Fail!")
@@ -28,5 +33,20 @@ def testonecore():
 def testomp():
     runtest_lz4chunkdecoders(bslz4decoders.omp_lz4)
 
+def runtest_lz4blockdecoders( decoder ):
+    for h5name, dset in testcases:
+        # reference data
+        ref = read_chunks.get_frame_h5py( h5name, dset, 0 )
+        (filtered, byteschunk), shp, dtyp = read_chunks.get_chunk( h5name, dset, 0 )
+        chunk = np.frombuffer(byteschunk, np.uint8)
+        out = np.empty( shp[0]*shp[1], dtyp )
+        blocksize, blocks = read_chunks.get_blocks( chunk, shp, dtyp )
+        decoder( chunk, dtyp.itemsize, blocksize, blocks, out.view( np.uint8 ) )
+        decoded = bitshuffle.bitunshuffle( out )
+        if not (decoded == ref.ravel()).all():
+            print("Fail!")
+            print(decoded)
+            print(ref.ravel())
 
-
+def testompblocked():
+    runtest_lz4blockdecoders( bslz4decoders.omp_lz4_blocks )
