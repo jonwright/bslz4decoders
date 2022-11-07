e16cddeac39c4e785936c27e2d60bf93a6b32911
Jon Wright <jonathan.wright@gmail.com>
2022-11-07 20:19:25 +0100

    streaming and cleanup

--- bslz4decoders/test/iter_h5chunk_stream.py
+++ bslz4decoders/test/iter_h5chunk_stream.py
@@ -1,14 +1,17 @@
 
 from bslz4decoders.lz4dc_cuda import BSLZ4CUDA
-from bslz4decoders.read_chunks import iter_chunks, get_frame_h5py
+from bslz4decoders.read_chunks import iter_h5chunks, iter_chunks, get_frame_h5py
 from bslz4decoders.test.testcases import testcases as TESTCASES
 
 from timeit import default_timer
 import numpy as np, pylab as pl
 from pycuda import gpuarray
+import pycuda.driver
 from pycuda.reduction import ReductionKernel
 
-
+import pycuda.tools
+memorypool = pycuda.tools.PageLockedMemoryPool()
+gpumempool = pycuda.tools.DeviceMemoryPool()
 
 def check_and_show( a1, a2 ):
     assert a1.shape == a2.shape, str(a1.shape)+str(a2.shape)
@@ -38,13 +41,18 @@ def testcuda(hname, dset):
         print("DtoH Hsum H5pyRead Hsum")
     else:
         print()
-    iterator = iter_chunks( hname, dset )
+        
+    iterator = iter_h5chunks( hname, dset, memory = pycuda.driver.pagelocked_empty(
+        (4096*4096*4,), np.uint8, mem_flags=pycuda.driver.host_alloc_flags.DEVICEMAP) )
     frm = 0
+    nb = 0
     while 1:
         t = [ default_timer()*1e3, ]
         try:
+            # todo : put this in a different thread. And only get next after
+            # you finish reading the data.
             config,chunk = next( iterator )
-        except StopIteration:
+        except:
             break
         t.append( default_timer()*1e3)
         blocks = config.get_blocks( chunk )
@@ -52,15 +60,20 @@ def testcuda(hname, dset):
         t.append( default_timer()*1e3)
         if dc is None:
             dc = BSLZ4CUDA( nbytes, config.dtype.itemsize, config.blocksize )
-            out_gpu = gpuarray.empty( config.shape, dtype = config.dtype )
+            out_gpu = gpuarray.empty( config.shape, dtype = config.dtype,
+                                          allocator= gpumempool.allocate )  
         else:
             if nbytes != out_gpu.nbytes:
-                out_gpu = gpuarray.empty( config.shape, dtype = config.dtype )
+                out_gpu = gpuarray.empty( config.shape, dtype = config.dtype,
+                                          allocator= gpumempool.allocate )
+                                          
                 dc.reset( config.output_nbytes, config.dtype.itemsize, config.blocksize )
         t.append( default_timer()*1e3 )
         _ = dc( chunk, blocks, out_gpu )
         t.append( default_timer()*1e3 )
-        sgpu = gpu_sums[ config.dtype ]( out_gpu ).get()
+        sgpu = gpu_sums[ config.dtype ]( out_gpu, stream=dc.stream )
+        t.append( default_timer()*1e3 )
+        sgpu = sgpu.get()
         t.append( default_timer()*1e3 )
         if __debug__:
             data = out_gpu.get()
@@ -69,16 +82,19 @@ def testcuda(hname, dset):
             ref = get_frame_h5py( hname, dset, frm )
             t.append( default_timer()*1e3 )
             sref = ref.ravel().sum(dtype = np.int64)
-            t.append( default_timer()*1e3 )
+            t.append( default_timer()*1e3 ) 
             check_and_show( ref, data )
             assert(sref == sdata),  " ".join((repr(sref),repr(sdata)))
             assert(sref == sgpu),  " ".join((repr(sref),repr(sdata)))
         dt = [t[i]-t[i-1] for i in range(1,len(t))]
         print(("%.3f ms, "*len(dt))%tuple(dt), hname, dset )
         frm += 1
+        nb += config.output_nbytes
         if frm == 1:
             t1 = default_timer()*1e3
     print("Total time after startup",t[-1]-t0,"ms", t[-1]-t1,'ms after init')
+    print("Total GB",nb/1e9,"speed ~ %.3f GB/s  asymptotic ~ %.3f GB/s"%((nb/1e6)/(t[-1]-t0),
+          (nb - config.output_nbytes)/1e6/(t[-1]-t1)))
 
 if __name__=="__main__":
     import sys
--- bslz4decoders/test/run_tests.sh
+++ bslz4decoders/test/run_tests.sh
@@ -1,9 +1,11 @@
 
 
-python test_readchunks.py
-python test_decoders.py
-python test_q.py
-python test_h5chunk.py
-python test_bench_read.py
-python test_cuda.py
-python test_itercuda.py /data/id11/nanoscope/blc12454/id11/WAu5um/WAu5um_DT3/scan0001/eiger_0000.h5 /entry_0000/ESRF-ID11/eiger/data
+python3 test_readchunks.py
+python3 test_decoders.py
+python3 test_q.py
+python3 test_h5chunk.py
+python3 test_bench_read.py
+python3 test_cuda.py
+python3 test_itercuda.py hplc.h5 /entry_0000/measurement/data
+python3 test_iter_h5chunk_stream.py hplc.h5 /entry_0000/measurement/data
+python3 -O iter_h5chunk_stream.py hplc.h5 /entry_0000/measurement/data
